3a49fc47c1f01a5cef9322c5732160446891e688
eva2694 <eu2694@student.uni-lj.si>
2024-04-12 09:23:02 +0200

    Initial activitie

---
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/ChooseCategoryActivity.java
@@ -0,0 +1,14 @@
+package si.uni_lj.fe.tnuv.cppaplikacija;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_category);
+    }
+}
---
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/ResultsActivity.java
@@ -0,0 +1,14 @@
+package si.uni_lj.fe.tnuv.cppaplikacija;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
