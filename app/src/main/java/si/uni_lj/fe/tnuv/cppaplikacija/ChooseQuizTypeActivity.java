aff316ebd6d36feb7f5ed8d8d65cebdeef68d8d6
eva2694 <eu2694@student.uni-lj.si>
2024-04-15 00:36:29 +0200

    povezava activty za izbiro k viza s kvizom in seznamom vprasanj

--- app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/ChooseQuizTypeActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/ChooseQuizTypeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,18 +14,39 @@ public class ChooseQuizTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_quiz_type);
+        final int[] categoryId = {1};
+        int imageRID;
+        final String[] categoryTitle = {getResources().getString(R.string.category_title)};
 
-        // dobimo naslov in id izbrane kategorije
+        // dobimo naslov, sliko in id izbrane kategorije
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("category_title") && intent.hasExtra("category_id")) {
-            String categoryTitle = intent.getStringExtra("category_title");
-            int categoryId = intent.getIntExtra("category_id", 1);
-            int imageRID = intent.getIntExtra("image", R.drawable.placeholder_image);
+            categoryTitle[0] = intent.getStringExtra("category_title");
+            categoryId[0] = intent.getIntExtra("category_id", 1);
+            imageRID = intent.getIntExtra("image", R.drawable.placeholder_image);
 
             TextView catTitleView = findViewById(R.id.chooseQuizCatTitle);
             ImageView image = findViewById(R.id.imageView);
-            catTitleView.setText(categoryTitle);
+            catTitleView.setText(categoryTitle[0]);
             image.setImageResource(imageRID);
         }
+
+        // gumb -> mešani kviz
+        Button mixQuizButton = findViewById(R.id.button_mix_quiz);
+        mixQuizButton.setOnClickListener(v -> {
+            Intent quizIntent = new Intent(ChooseQuizTypeActivity.this, QuizActivity.class);
+            quizIntent.putExtra("category_title", categoryTitle[0]);
+            quizIntent.putExtra("category_id", categoryId[0]);
+            startActivity(quizIntent);
+        });
+
+        // gumb -> seznam vseh vprašanj
+        Button listQuestionsButton = findViewById(R.id.button_list_questions);
+        listQuestionsButton.setOnClickListener(v -> {
+            Intent listIntent = new Intent(ChooseQuizTypeActivity.this, QuestionListActivity.class);
+            listIntent.putExtra("category_title", categoryTitle[0]);
+            listIntent.putExtra("category_id", categoryId[0]);
+            startActivity(listIntent);
+        });
     }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
@@ -2,13 +2,21 @@ package si.uni_lj.fe.tnuv.cppaplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class QuizActivity extends AppCompatActivity {
-
+    private int categoryId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // dobimo naslov, in id izbrane kategorije
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("category_title") && intent.hasExtra("category_id")) {
+            String categoryTitle = intent.getStringExtra("category_title");
+            categoryId = intent.getIntExtra("category_id", 1);
+        }
     }
 }
