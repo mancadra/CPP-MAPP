6179f05d33c84afb94e4916ab57373c188753d76
eva2694 <eu2694@student.uni-lj.si>
2024-05-02 13:54:10 +0200

    quiz funkcionalnosti

--- app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +18,9 @@ public class QuizActivity extends AppCompatActivity {
     private int categoryId;
     private String type;
     private int questionId;
+    private ArrayList<Question>[] questions;
+    private int currentQuestionIndex = 0;
+    private int randomQuestionCount = 0;
     DatabaseManager databaseManager = new DatabaseManager();
 
     @Override
@@ -55,21 +61,20 @@ public class QuizActivity extends AppCompatActivity {
             TextView catTitle = findViewById(R.id.catTitleInQuiz);
             catTitle.setText(categoryTitle);
 
-            if(type.equals("mix")) {
-                getRandomQuestion(categoryId);
-            } else {
-                getQuestion(categoryId, questionId);
-            }
+            getQuestions(categoryId);
         }
     }
 
-    private void getQuestion(int categoryId, int questionId) {
+    private void getQuestions(int categoryId) {
         Log.d("QuizActivity","Specific question requested.");
 
         // Dobimo vprašanja iz kategorije
         databaseManager.getCategoryQuestions(categoryId, questionList ->  {
             if (questionList != null && questionList.length > 0 && questionList[0] != null) {
                 Log.d("QuizActivity", "Data fetched successfully: " + questionList[0].size() + " questions");
+                questions = questionList;
+                currentQuestionIndex = getFirstQuestionIndex();
+                displayQuestion();
             } else {
                 Log.e("QuizActivity", "No questions fetched or questions array is null.");
             }
@@ -77,10 +82,91 @@ public class QuizActivity extends AppCompatActivity {
 
     }
 
-    private void getRandomQuestion(int categoryId) {
-        Log.d("QuizActivity","Random question requested.");
-
+    private void displayQuestion() {
+        if (questions != null && !questions[0].isEmpty()) {
+            Question currentQuestion;
+            if (type.equals("mix")) {
+                currentQuestion = getRandomQuestion();
+            } else {
+                currentQuestion = getSpecificQuestion();
+            }
+            if(currentQuestion != null) updateUI(currentQuestion);
+        }
     }
+
+    private Question getSpecificQuestion() {
+        // Poiščemo indeks vprašanja z ID-jem
+        int index = currentQuestionIndex;
+        currentQuestionIndex++;
+
+        if (index >= 0 && index < questions[0].size()) {
+            return questions[0].get(index);
+        } else {
+            // Če ID vprašanja ni, vrnemo prvo vprašanje
+            return questions[0].get(0);
+        }
+    }
+
+    private int getFirstQuestionIndex() {
+        int index = -1;
+
+        if(questions == null) {
+            Log.e("QuizActivity", "questions array is null");
+            return -1;
+        }
+
+        for (int i = 0; i < questions[0].size(); i++) {
+            if (questions[0].get(i).getId() == questionId) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    private Question getRandomQuestion() {
+        randomQuestionCount++;
+        if (randomQuestionCount <= 10) {
+            // // Generiramo random indeks
+            int randomIndex = (int) (Math.random() * questions[0].size());
+            return questions[0].get(randomIndex);
+        } else {
+            // Zaključimo kviz, če je bilo prikazanih 10  vprašanj
+            finishQuiz();
+            return null;
+        }
+    }
+
+    private void updateUI(Question question) {
+        // Posodobimo UI
+        TextView questionTextView = findViewById(R.id.tv_question);
+        questionTextView.setText(question.getQuestionText());
+
+        Button nextButton = findViewById(R.id.btn_next_question);
+        nextButton.setEnabled(true);
+    }
+
+    private void finishQuiz() {
+        // Začnemo ResultsActivity za prikaz rezultatov
+        Intent intent = new Intent(QuizActivity.this, ResultsActivity.class);
+
+        startActivity(intent);
+        // Zaključimo QuizActivity
+        finish();
+    }
+
+
+    public void onNextButtonClick(View view) {
+        // Onemogočimo gumb "Naprej", preprečimo večkratne klike
+        Button nextButton = findViewById(R.id.btn_next_question);
+        nextButton.setEnabled(false);
+
+
+        displayQuestion();
+    }
+
+
 }
 
 /* todo:
