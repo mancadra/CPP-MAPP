b2dfe7d2a2b201a2cfa9220e97e360dc762aa7f1
eva2694 <eu2694@student.uni-lj.si>
2024-05-03 11:58:42 +0200

    funkcionalnost btn check

--- app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/QuizActivity.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
@@ -17,6 +17,7 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuizActivity extends AppCompatActivity {
     private int categoryId;
@@ -25,12 +26,20 @@ public class QuizActivity extends AppCompatActivity {
     private ArrayList<Question>[] questions;
     private int currentQuestionIndex = 0;
     private int randomQuestionCount = 0;
+    boolean[] selectedAnswers;
+    int[] correctAnswers;
     DatabaseManager databaseManager = new DatabaseManager();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // Disable gumb preveri
+        Button checkButton = findViewById(R.id.btn_check_answer);
+        checkButton.setEnabled(false);
 
         // nastavimo navbar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -104,9 +113,11 @@ public class QuizActivity extends AppCompatActivity {
         currentQuestionIndex++;
 
         if (index >= 0 && index < questions[0].size()) {
+            correctAnswers = questions[0].get(index).getCorrectAnswers();
             return questions[0].get(index);
         } else {
             // Če ID vprašanja ni, vrnemo prvo vprašanje
+            correctAnswers = questions[0].get(0).getCorrectAnswers();
             return questions[0].get(0);
         }
     }
@@ -134,6 +145,7 @@ public class QuizActivity extends AppCompatActivity {
         if (randomQuestionCount <= 10) {
             // // Generiramo random indeks
             int randomIndex = (int) (Math.random() * questions[0].size());
+            correctAnswers = questions[0].get(randomIndex).getCorrectAnswers();
             return questions[0].get(randomIndex);
         } else {
             // Zaključimo kviz, če je bilo prikazanih 10  vprašanj
@@ -143,6 +155,9 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateUI(Question question) {
+        // Gumb preveri
+        Button checkButton = findViewById(R.id.btn_check_answer);
+        checkButton.setEnabled(false);
         // Tekst vprašanje
         TextView questionTextView = findViewById(R.id.tv_question);
         questionTextView.setText(question.getQuestionText());
@@ -152,9 +167,11 @@ public class QuizActivity extends AppCompatActivity {
         layoutAnswers.removeAllViews();
 
         String[] answers = question.getAnswers();
+        selectedAnswers = new boolean[answers.length];
+        Arrays.fill(selectedAnswers, false);
 
-
-        for (String answer : answers) {
+        for (int i = 0; i < answers.length; i++) {
+            String answer = answers[i];
             CustomGradientDrawable customDrawable = new CustomGradientDrawable(
                     Color.parseColor("#6804ec"),
                     Color.parseColor("#6804ec"),
@@ -168,10 +185,16 @@ public class QuizActivity extends AppCompatActivity {
             answerButton.setText(answer);
             answerButton.setBackground(customDrawable);
             answerButton.setTextColor(Color.WHITE);
-            // ob kliku se spremeni barva
-            answerButton.setOnClickListener(view -> {
-                Drawable backgroundDrawable = answerButton.getBackground();
+            answerButton.setTag(i);
 
+
+            // ob kliku se spremeni barva OBROBE odgovora
+
+            answerButton.setOnClickListener(view -> {
+                int index = (int) view.getTag();
+                selectedAnswers[index] = !selectedAnswers[index];
+
+                Drawable backgroundDrawable = answerButton.getBackground();
                 if (backgroundDrawable instanceof CustomGradientDrawable) {
                     CustomGradientDrawable customDrawable2 = (CustomGradientDrawable) backgroundDrawable;
 
@@ -183,15 +206,19 @@ public class QuizActivity extends AppCompatActivity {
                     int newStrokeColor = (currentStrokeColor == color1) ? color2 : color1;
 
                     customDrawable.setStrokeColor(newStrokeColor);
-
                     answerButton.setBackground(customDrawable);
                 }
+
+                // Gumb preveri
+                checkButton.setEnabled(true);
             });
 
 
 
             layoutAnswers.addView(answerButton);
         }
+
+
 
         // Gumb naprej
         Button nextButton = findViewById(R.id.btn_next_question);
@@ -215,6 +242,53 @@ public class QuizActivity extends AppCompatActivity {
 
 
         displayQuestion();
+    }
+
+    public void onCheckButtonClick(View view) {
+        LinearLayout layoutAnswers = findViewById(R.id.layout_answers);
+        // array za pravilne odgovore
+        boolean[] correctness  = new boolean[selectedAnswers.length];
+        Arrays.fill(correctness, false);
+
+        for (int i = 0; i < selectedAnswers.length; i++) {
+            for (int correctAnswer : correctAnswers) {
+                if (i == correctAnswer) {
+                    correctness[i] = true;
+                    break;
+                }
+            }
+        }
+
+        // iteracija po gumbih
+        for (int i = 0; i < layoutAnswers.getChildCount(); i++) {
+            View childView = layoutAnswers.getChildAt(i);
+            if (childView instanceof Button) {
+
+                Button answerButton = (Button) childView;
+                // če uporabnik odgovoril pravilno
+                if (correctness[i]) {
+                    if(selectedAnswers[i]) {
+
+                        int bgColor = Color.parseColor("#008000");
+                        int strokeColor = Color.parseColor("#ffbb00");
+                        CustomGradientDrawable customDrawable = new CustomGradientDrawable(bgColor, strokeColor, 10);
+                        answerButton.setBackground(customDrawable);
+
+                    } else {
+                        answerButton.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC);
+                    }
+                // če je uporabnik odgovoril nepravilno
+                } else {
+                    if (selectedAnswers[i]) {
+                        int bgColor = Color.RED;
+                        int strokeColor = Color.parseColor("#ffbb00");
+                        CustomGradientDrawable customDrawable = new CustomGradientDrawable(bgColor, strokeColor, 10);
+                        answerButton.setBackground(customDrawable);
+                    }
+                }
+
+            }
+        }
     }
 
 
