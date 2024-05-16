39a33a35a696b37a18a2aca7448242d3456e8f7f
eva2694 <eu2694@student.uni-lj.si>
2024-05-16 19:18:03 +0200

    pie chart

--- app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/HistoryActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/cppaplikacija/HistoryActivity.java
@@ -3,15 +3,24 @@ package si.uni_lj.fe.tnuv.cppaplikacija;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
     private PreferencesManager preferencesManager;
     private TextView tvCorrectAnswers, tvIncorrectAnswers, tvTotalAnswers, tvTotal;
+    private int totalCorrect, totalIncorrect, totalUnanswered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +58,35 @@ public class HistoryActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        // tortni diagram
+        PieChart pieChart = findViewById(R.id.pieChart);
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(totalCorrect, "Pravilno"));
+        entries.add(new PieEntry(totalIncorrect, "Nepravilno"));
+        entries.add(new PieEntry(totalUnanswered, "Neodgovorjeno"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Answers");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataSet.setValueTextSize(12f);
+
+        PieData pieData = new PieData(dataSet);
+
+// Customize legend
+        Legend legend = pieChart.getLegend();
+        legend.setTextSize(14f);
+
+        pieChart.setData(pieData);
+        pieChart.invalidate(); // Refresh chart
     }
 
     // pridobi podatke iz shared preferences
     private void updateQuestionStats() {
         int totalQuestions = QuestionsSingleton.getInstance().getTotalNumberOfQuestions();
         int totalAnswered = preferencesManager.getTotalAnsweredQuestions();
-        int totalCorrect = preferencesManager.getTotalCorrectlyAnsweredQuestions();
-        int totalIncorrect = preferencesManager.getTotalFalselyAnsweredQuestions();
+        totalCorrect = preferencesManager.getTotalCorrectlyAnsweredQuestions();
+        totalIncorrect = preferencesManager.getTotalFalselyAnsweredQuestions();
+        totalUnanswered = totalQuestions - totalAnswered;
 
         String correctAnswersText = getString(R.string.correct_answers, totalCorrect);
         String incorrectAnswersText = getString(R.string.incorrect_answers, totalIncorrect);
